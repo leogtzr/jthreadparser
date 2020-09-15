d8a9694dfb9276bf7cf4638a7377418052844c43
leogtzr <leogutierrezramirez@gmail.com>
2020-09-14 19:38:51 -0600

    add example #1

--- examples/simpleusage/main.go
+++ examples/simpleusage/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/leogtzr/jthreadparser"
+)
+
 func main() {
+
+	threadDumpFile := "../../threaddumpsamples/9.0.4.0-test.txt"
+
+	threads, err := jthreadparser.ParseFromFile(threadDumpFile)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, th := range threads {
+		fmt.Printf("'%s' (%s) (%s) [%s]\n", th.Name, th.ID, th.NativeID, th.State)
+	}
 
 }
